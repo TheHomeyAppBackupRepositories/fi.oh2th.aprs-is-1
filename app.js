0272ad26421ebc4b9bfad5724159362705d02d99
Your Name <you@example.com>
2024-08-10 20:52:17 +0200

    Update 1.0.10 to 1.1.0

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 'use strict';
 
 const Homey = require('homey');
-const { Log } = require('homey-log');
+const process = require('process');
+
 const flowActions = require('./lib/flows/actions');
 
+process.on('unhandledRejection', (reason, promise) => {
+	console.error(`${new Date().toISOString()} [log] [process] Unhandled Rejection: ${promise}, reason: ${reason}`);
+	// You can add additional error handling or logging here.
+});
 module.exports = class AprsApp extends Homey.App {
 
-  /**
-   * onInit is called when the app is initialized.
-   */
-  async onInit() {
-    this.myAppIdVersion = `${this.homey.manifest.id}/${this.homey.manifest.version}`;
-    this.homeyLog = new Log({ homey: this.homey });
+	/**
+	 * onInit is called when the app is initialized.
+	 */
+	async onInit() {
+		this.myAppIdVersion = `${this.homey.manifest.id}/${this.homey.manifest.version}`;
 
-    this.log(`${this.myAppIdVersion} - onInit - starting...`);
+		this.log(`${this.myAppIdVersion} - onInit - starting...`);
 
-    await flowActions.init(this);
+		await flowActions.init(this);
 
-    this.log(`${this.myAppIdVersion} - onInit - started.`);
-  }
+		this.log(`${this.myAppIdVersion} - onInit - started.`);
+	}
 
 };
